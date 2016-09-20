b34de5788702e68d72beec72705e56b755b97675
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2016-09-20 10:15:47 +0200

    refined validation metrics output

--- experiments/patchSegmentation/segmentPatches_UNet_v0.2_lossSampling_validate.py
+++ experiments/patchSegmentation/segmentPatches_UNet_v0.2_lossSampling_validate.py
@@ -27,7 +27,17 @@ def softmax_helper(x):
     e_x = theano.tensor.exp(x - x.max(axis=1, keepdims=True))
     return e_x / e_x.sum(axis=1, keepdims=True)
 
-def predict_whole_dataset(patient_id):
+
+validation_patients = np.unique([ 75,   1,  67,   1, 127, 120,  94, 131,  78,  74,  62,  10,  65, 47, 124])
+experiment_name = "segment_tumor_v0.2_Unet_lossSampling"
+results_folder = "/home/fabian/datasets/Hirntumor_von_David/experiments/results/%s/" % experiment_name
+epoch = 4
+
+for patient_id in validation_patients:
+    print patient_id
+    output_folder = os.path.join(results_folder, "%03.0d" % patient_id)
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
     t1km_img, flair_img, adc_img, cbv_img, t1km_downsampled, seg_combined = load_patient_resampled(patient_id)
     import matplotlib.pyplot as plt
     assert t1km_img.shape == flair_img.shape
@@ -41,13 +51,14 @@ def predict_whole_dataset(patient_id):
     cbv_img = cbv_img[:, :shape_0_new, :shape_1_new]
     seg_combined = seg_combined[:, :shape_0_new, :shape_1_new]
 
+    print "compiling theano functions"
     # uild_UNet(n_input_channels=1, BATCH_SIZE=None, num_output_classes=2, pad='same', nonlinearity=lasagne.nonlinearities.elu, input_dim=(128, 128), base_n_filters=64, do_dropout=False):
     net = UNet.build_UNet(20, 1, 5, input_dim=(t1km_img.shape[1], t1km_img.shape[2]), base_n_filters=16)
     output_layer = net["output_segmentation"]
-    with open("/home/fabian/datasets/Hirntumor_von_David/experiments/results/segmentPatches_UNet_v0.2_lossSampling/segment_tumor_v0.2_Unet_lossSampling_allLossesNAccur_ep4.pkl", 'r') as f:
+    with open(os.path.join(results_folder, "%s_allLossesNAccur_ep%d.pkl" % (experiment_name, epoch)), 'r') as f:
         tmp = cPickle.load(f)
 
-    with open("/home/fabian/datasets/Hirntumor_von_David/experiments/results/segmentPatches_UNet_v0.2_lossSampling/segment_tumor_v0.2_Unet_lossSampling_Params_ep4.pkl", 'r') as f:
+    with open(os.path.join(results_folder, "%s_Params_ep%d.pkl" % (experiment_name, epoch)), 'r') as f:
         params = cPickle.load(f)
         lasagne.layers.set_all_param_values(output_layer, params)
 
@@ -57,6 +68,7 @@ def predict_whole_dataset(patient_id):
     output = softmax_helper(lasagne.layers.get_output(output_layer, data_sym, deterministic=False))
     pred_fn = theano.function([data_sym], output)
 
+    print "predicting image"
     cmap = ListedColormap([(0,0,0), (0,0,1), (0,1,0), (1,0,0), (1,1,0)])
 
     z = 2
@@ -75,12 +87,28 @@ def predict_whole_dataset(patient_id):
     seg_combined = seg_combined[2:t1km_img.shape[0]-2]
     res_img = res.argmax(-1)
 
+    print "calculating metrics"
     acc, metrics_by_class = calculate_validation_metrics(res, seg_combined, {0: 'background', 1: 'brain', 2: 'edema', 3: 'ce_tumor', 4: 'necrosis'})
+    with open(os.path.join(output_folder, "validation_metrics.txt"), 'w') as f:
+        f.write("The overall accuracy on this dataset was: \t%f\n\n" % acc)
+        for c in metrics_by_class.keys():
+            f.write("Results for label: %s\n" % c)
+            for metric in metrics_by_class[c].keys():
+                f.write("%s: \t%f\n" % (metric, metrics_by_class[c][metric]))
+            f.write("\n")
 
-    for i in xrange(t1km_img.shape[0]):
-        res_img[i][0,0:5] = [0,1,2,3,4]
-        seg_combined[i][0,0:5] = [0,1,2,3,4]
-        errors = seg_combined[i] == res_img[i]
+    output_folder_images = os.path.join(output_folder, "segmentation_slices")
+    if not os.path.isdir(output_folder_images):
+        os.mkdir(output_folder_images)
+
+    np.save(os.path.join(output_folder, "seg_gt.npy"), seg_combined)
+    np.save(os.path.join(output_folder, "seg_pred.npy"), res_img)
+
+    print "writing segmentation images"
+    for i in range(2, t1km_img.shape[0]-2):
+        res_img[i-2][0,0:5] = [0,1,2,3,4]
+        seg_combined[i-2][0,0:5] = [0,1,2,3,4]
+        errors = seg_combined[i-2] == res_img[i-2]
         errors[0, 0:2] = [True, False]
         plt.figure(figsize=(24,12))
         plt.subplot(2,4,1)
@@ -92,10 +120,10 @@ def predict_whole_dataset(patient_id):
         plt.subplot(2,4,4)
         plt.imshow(cbv_img[i], cmap="gray")
         plt.subplot(2,4,5)
-        plt.imshow(res_img[i], cmap=cmap)
+        plt.imshow(res_img[i-2], cmap=cmap)
         plt.subplot(2,4,6)
-        plt.imshow(seg_combined[i], cmap=cmap)
+        plt.imshow(seg_combined[i-2], cmap=cmap)
         plt.subplot(2,4,7)
-        plt.imshow(seg_combined[i] == res_img[i], cmap="gray")
-        plt.savefig("/home/fabian/datasets/Hirntumor_von_David/experiments/some_images/patient%d_segWholeDataset_z%03.0f"%(patient_id, i))
+        plt.imshow(seg_combined[i-2] == res_img[i-2], cmap="gray")
+        plt.savefig(os.path.join(output_folder_images, "patient%d_segWholeDataset_z%03.0f"%(patient_id, i)))
         plt.close()
